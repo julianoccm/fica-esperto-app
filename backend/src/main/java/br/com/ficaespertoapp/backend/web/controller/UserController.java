d33585433e65424a37c2f0b1a7208a4f50bd138b
julianoccm <julianocolerecmoreira@gmail.com>
2025-05-27 14:49:18 -0300

    [feat][back] Code cleanup

--- backend/src/main/java/br/com/ficaespertoapp/backend/web/controller/UserController.java
+++ backend/src/main/java/br/com/ficaespertoapp/backend/web/controller/UserController.java
@@ -1,15 +1,10 @@
 package br.com.ficaespertoapp.backend.web.controller;
 
 
-import br.com.ficaespertoapp.backend.domain.dto.LoginDTO;
-import br.com.ficaespertoapp.backend.domain.dto.UserDTO;
 import br.com.ficaespertoapp.backend.domain.service.UserService;
-import br.com.ficaespertoapp.backend.web.security.jwt.JwtTokenGenerator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
