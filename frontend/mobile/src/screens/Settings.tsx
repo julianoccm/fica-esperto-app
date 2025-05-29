ce056e2919a77f2af1dba508c800e9bbc292554a
julianoccm <julianocolerecmoreira@gmail.com>
2025-05-29 20:54:31 -0300

    [feat][mobile] Organize imports

--- frontend/mobile/src/screens/Settings.tsx
+++ frontend/mobile/src/screens/Settings.tsx
@@ -1,45 +1,32 @@
 import { useNavigation, type NavigationProp } from "@react-navigation/native";
-import {
-  ActivityIndicator,
-  Dimensions,
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ScrollView,
-} from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useEffect, useState } from "react";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import type { Post } from "../models/post";
 import type { NavigationStackParamList } from "../config/navigation-stack-param";
-import { UserData } from "../models/user-data";
-import { formatDate } from "../utils/date-utils";
-import UserService from "../services/user-service";
-import PostService from "../services/post-service";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function SettingsScreen() {
   const navigation = useNavigation<NavigationProp<NavigationStackParamList>>();
 
   const _logout = () => {
-    AsyncStorage.removeItem("token")
-    AsyncStorage.removeItem("user ")
+    AsyncStorage.removeItem("token");
+    AsyncStorage.removeItem("user ");
 
-    navigation.navigate("AuthControl")
-  }
+    navigation.navigate("AuthControl");
+  };
 
   const _goBack = () => {
-    navigation.goBack()
-  }
+    navigation.goBack();
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ height: 20 }} />
 
-      <TouchableOpacity style={{alignSelf: "flex-start", marginBottom: 30}} onPress={_goBack}>
+      <TouchableOpacity
+        style={{ alignSelf: "flex-start", marginBottom: 30 }}
+        onPress={_goBack}
+      >
         <Ionicons name="chevron-back" size={24} color="black" />
       </TouchableOpacity>
 
@@ -71,7 +58,7 @@ const styles = StyleSheet.create({
     width: "100%",
     borderRadius: 5,
     alignItems: "center",
-    marginTop: 20
+    marginTop: 20,
   },
   buttonFormText: {
     color: "#fff",
